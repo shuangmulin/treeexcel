1bc963d178c08c971bc22e8b3fd083d6e1e6b34c
zhongbaolin <shuangmulin45@163.com>
2021-04-03 17:35:55 +0800

    初始化

--- src/test/java/org/baolin/AppTest.java
+++ src/test/java/org/baolin/AppTest.java
@@ -1,17 +1,9 @@
 package org.baolin;
 
-import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
 }
